e846ac1bc166b6601a13dce402ec008f3a8582c4
lrodeveloperr <lrodeveloperr@gmail.com>
2026-09-01 08:36:53 -0400

    Enable Android notification desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -17,6 +18,7 @@ android {
         applicationId = "com.goodusestudios.weldinggaswallet"
         minSdk = 26
         targetSdk = flutter.targetSdkVersion
+        multiDexEnabled = true
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -26,6 +28,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 kotlin {
